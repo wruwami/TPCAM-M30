3c73f8dd24ae8939883b496c3781d6565534ef5f
boseok jung <wruwami@naver.com>
2022-11-03 11:17:08 +0900

    IndicatorWidget 수정

--- IndicatorWidget.cpp
+++ IndicatorWidget.cpp
@@ -2,10 +2,18 @@
 #include "ui_IndicatorWidget.h"
 #include "CustomPushButton.h"
 
+#include <QFile>
+
 IndicatorWidget::IndicatorWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::IndicatorWidget)
 {
+    QFile file(":/style/indicatorWidget.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QString::fromLatin1(file.readAll());
+
+    setStyleSheet(styleSheet);
+
     ui->setupUi(this);
     ui->onPushButton->setVisible(false);
     ui->offPushButton->setVisible(false);
@@ -56,6 +64,12 @@ void IndicatorWidget::on_enforcementPushButton_clicked()
 void IndicatorWidget::on_weatherPushButton_clicked()
 {
     clearSecondForuthRow();
+    m_pSunnyPushButton = new CustomPushButton();
+    m_pRainyPushButton = new CustomPushButton();
+
+//    ui->blankWidget->setAcceptDrops();
+    ui->gridLayout->addWidget(m_pSunnyPushButton, 1, 1);
+    ui->gridLayout->addWidget(m_pRainyPushButton , 1, 2);
 }
 
 void IndicatorWidget::clearSecondForuthRow()

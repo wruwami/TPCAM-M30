8ef3d34a55e43ead2d2031f1f37f56bc7707a118
boseok jung <wruwami@naver.com>
2022-11-07 16:14:30 +0900

    settingwidget frame 추가

--- SettingWidget.cpp
+++ SettingWidget.cpp
@@ -12,3 +12,57 @@ SettingWidget::~SettingWidget()
 {
     delete ui;
 }
+
+void SettingWidget::on_setting1PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_setting2PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_setting3PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_setting4PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_setting5PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_setting6PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_setting7PushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_savePushButton_clicked()
+{
+
+}
+
+
+void SettingWidget::on_cancelPushButton_clicked()
+{
+
+}
+
--- SettingWidget.h
+++ SettingWidget.h
@@ -15,6 +15,25 @@ public:
     explicit SettingWidget(QWidget *parent = 0);
     ~SettingWidget();
 
+private slots:
+    void on_setting1PushButton_clicked();
+
+    void on_setting2PushButton_clicked();
+
+    void on_setting3PushButton_clicked();
+
+    void on_setting4PushButton_clicked();
+
+    void on_setting5PushButton_clicked();
+
+    void on_setting6PushButton_clicked();
+
+    void on_setting7PushButton_clicked();
+
+    void on_savePushButton_clicked();
+
+    void on_cancelPushButton_clicked();
+
 private:
     Ui::SettingWidget *ui;
 };

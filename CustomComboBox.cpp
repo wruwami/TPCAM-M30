d57e31c9b5db83a29a14e057b0085b02cc4a7663
boseok jung <wruwami@naver.com>
2022-12-07 13:22:31 +0900

    중간 내용 저장

--- CustomComboBox.cpp
+++ CustomComboBox.cpp
@@ -1,10 +1,47 @@
 #include "CustomComboBox.h"
 
+#include <QStyledItemDelegate>
+#include <QProxyStyle>
+
 #include "FontSize.h"
+
+class AlignDelegate: public QStyledItemDelegate{
+public:
+    AlignDelegate(Qt::Alignment alignment, QObject *parent = nullptr):
+        QStyledItemDelegate(parent), m_alignment(alignment)
+    {
+
+    }
+protected:
+    void initStyleOption(QStyleOptionViewItem *option, const QModelIndex &index) const
+    {
+        QStyledItemDelegate::initStyleOption(option, index);
+        option->displayAlignment = m_alignment;
+    }
+    Qt::Alignment m_alignment;
+};
+
+class AlignComboBoxProxy: public QProxyStyle{
+public:
+    using QProxyStyle::QProxyStyle;
+    void drawItemText(QPainter *painter,
+                      const QRect &rect,
+                      int /*flags*/,
+                      const QPalette &pal,
+                      bool enabled,
+                      const QString &text,
+                      QPalette::ColorRole textRole) const
+    {
+
+        QProxyStyle::drawItemText(painter, rect, Qt::AlignCenter, pal, enabled, text, textRole);
+    }
+};
 
 CustomComboBox::CustomComboBox(QWidget *parent) : QComboBox(parent)
 {
     this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+//    setStyle(new AlignComboBoxProxy);
+//    setItemDelegate(new AlignDelegate(Qt::AlignCenter, this));
 }
 
 void CustomComboBox::resizeEvent(QResizeEvent *event)

7689dfd1346521e4233eda155cb45f9d7e53e465
boseok jung <wruwami@naver.com>
2022-12-15 12:44:46 +0900

    indicator 관련 수정

--- IndicatorCameraExposeWidget.cpp
+++ IndicatorCameraExposeWidget.cpp
@@ -11,10 +11,10 @@ IndicatorCameraExposeWidget::IndicatorCameraExposeWidget(QWidget *parent) :
     ui->setupUi(this);
 
     setWindowFlags(Qt::FramelessWindowHint);
-    setAttribute(Qt::WA_TranslucentBackground);
+//    setAttribute(Qt::WA_TranslucentBackground);
 
 //    setGeometry(GetWidgetSizePos(QRect(0, 125, 1600, 960)));
-    setGeometry(GetWidgetSizePos(QRect(0, 272, 1600, 563)));
+    setGeometry(GetWidgetSizePos(QRect(0, 272 + 125, 1600, 563)));
 
     ui->showHidePushButton->setText(LoadString("IDS_HIDE"));
     ui->speedPushButton->setText(LoadString("IDS_SPEED"));
--- IndicatorCameraFocusWidget.cpp
+++ IndicatorCameraFocusWidget.cpp
@@ -11,9 +11,9 @@ IndicatorCameraFocusWidget::IndicatorCameraFocusWidget(QWidget *parent) :
     ui->setupUi(this);
 
     setWindowFlags(Qt::FramelessWindowHint);
-    setAttribute(Qt::WA_TranslucentBackground);
+//    setAttribute(Qt::WA_TranslucentBackground);
 //    setGeometry(GetWidgetSizePos(QRect(0, 125, 1600, 960)));
-    setGeometry(GetWidgetSizePos(QRect(0, 433, 1600, 402)));
+    setGeometry(GetWidgetSizePos(QRect(0, 433 + 125, 1600, 402)));
 
     ui->showHidePushButton->setText(LoadString("IDS_HIDE"));
     ui->speedPushButton->setText(LoadString("IDS_SPEED"));

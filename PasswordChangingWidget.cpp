e8402f77225329f3aa364256c80301a81e412310
boseok jung <wruwami@naver.com>
2022-12-19 13:37:29 +0900

    polishing

--- PasswordChangingWidget.cpp
+++ PasswordChangingWidget.cpp
@@ -1,14 +1,37 @@
 #include "PasswordChangingWidget.h"
 #include "ui_PasswordChangingWidget.h"
 
+#include <Qdialog>
+
+#include "StringLoader.h"
+
 PasswordChangingWidget::PasswordChangingWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::PasswordChangingWidget)
 {
     ui->setupUi(this);
+
+    m_pParent = (QDialog*)parent;
+
+    ui->currentLabel->setText(LoadString("IDS_CURRENT"));
+    ui->newLabel->setText(LoadString("IDS_NEW"));
+    ui->confirmLabel->setText(LoadString("IDS_CONFIRM"));
+
+    ui->okPushButton->setText(LoadString("IDS_OK"));
+    ui->cancelPushButton->setText(LoadString("IDS_CANCEL"));
 }
 
 PasswordChangingWidget::~PasswordChangingWidget()
 {
     delete ui;
+}
+
+void PasswordChangingWidget::on_okPushButton_clicked()
+{
+    m_pParent->accept();
+}
+
+void PasswordChangingWidget::on_cancelPushButton_clicked()
+{
+    m_pParent->reject();
 }
--- PasswordChangingWidget.h
+++ PasswordChangingWidget.h
@@ -14,6 +14,12 @@ class PasswordChangingWidget : public QWidget
 public:
     explicit PasswordChangingWidget(QWidget *parent = 0);
     ~PasswordChangingWidget();
+private:
+    QDialog* m_pParent;
+private slots:
+    void on_okPushButton_clicked();
+
+    void on_cancelPushButton_clicked();
 
 private:
     Ui::PasswordChangingWidget *ui;

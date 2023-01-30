11fcd350ccef2b58c4c63c21ecffa1e043870ff8
boseok jung <wruwami@naver.com>
2023-01-30 17:19:28 +0900

    ConfigManager 추가

---
+++ ConfigManager.cpp
@@ -0,0 +1,49 @@
+#include "ConfigManager.h"
+
+#include "qdir.h"
+#include <QDebug>
+#include <QFile>
+#include <QJsonParseError>
+
+#define DEFAULT_PATH    "settings"
+
+ConfigManager::ConfigManager(QString file_name)
+{
+    QDir qdir;
+
+    m_file_full_path = qdir.absolutePath() + "/" + DEFAULT_PATH + "/" + file_name;
+
+    QFile file(m_file_full_path);
+    file.open(QFile::ReadWrite);
+
+    if (!file.isOpen())
+    {
+        qDebug() << "no file has been opened";
+        file.close();
+        return;
+    }
+
+    QByteArray json = file.readAll();
+    QJsonParseError error{};
+    m_jsonDocument = QJsonDocument::fromJson(json, &error);
+    file.close();
+}
+
+QJsonObject ConfigManager::GetConfig()
+{
+    return m_jsonDocument.object();
+}
+
+void ConfigManager::SetConfig(QJsonObject jsonObject)
+{
+    m_jsonDocument = QJsonDocument(jsonObject);
+}
+
+void ConfigManager::SaveFile()
+{
+    QFile file(m_file_full_path);
+    file.open(QFile::WriteOnly);
+
+    file.write(m_jsonDocument.toJson());
+    file.close();
+}

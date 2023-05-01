37cf9267bb3163a84c15d143d15e0de973a58368
wruwami <wruwami@naver.com>
2023-05-01 12:58:10 +0900

    mod light

--- LightManager.cpp
+++ LightManager.cpp
@@ -4,14 +4,18 @@
 
 LightManager::LightManager()
 {
-    system("echo 154 | true || sudo tee -a /sys/class/gpio/export > /dev/null");
-    system("echo 154 > /sys/class/gpio/export");
-    system("echo out | sudo tee -a /sys/class/gpio/gpio154/direction > /dev/null");
+//    system("echo 154 | true || sudo tee -a /sys/class/gpio/export > /dev/null");
+//    system("echo 154 > /sys/class/gpio/export");
+//    system("echo out | sudo tee -a /sys/class/gpio/gpio154/direction > /dev/null");
 
 }
 
 void LightManager::SwapLight()
 {
+    system("echo 154 | true || sudo tee -a /sys/class/gpio/export > /dev/null");
+    system("echo 154 > /sys/class/gpio/export");
+    system("echo out | sudo tee -a /sys/class/gpio/gpio154/direction > /dev/null");
+
     if (isLightOn)
         system("echo 1 > /sys/class/gpio/gpio154/value");
     else

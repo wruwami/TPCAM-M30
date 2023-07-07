5e1d1956128b8c8863bf4451541015199b5ccf56
MINSUNG-KIM2 <mskim@comlaser.co.kr>
2023-07-07 14:56:31 +0900

    Update SelfTestWidget.cpp

--- SelfTestWidget.cpp
+++ SelfTestWidget.cpp
@@ -18,6 +18,7 @@
 #include "SerialViscaManager.h"
 #include "SerialPacket.h"
 #include "ViscaPacket.h"
+#include "ConfigManager.h"
 
 SelfTestWidget::SelfTestWidget(QWidget *parent) :
     QWidget(parent),
@@ -241,7 +242,13 @@ bool SelfTestWidget::BatteryTest()
     //moving average filter
     ltc.filterValues();
 
-    if (ltc.m_filteredVolt > 9.4)
+    // load setting_battery.json
+    ConfigManager config = ConfigManager("setting_battery.json");
+    QJsonObject object = config.GetConfig();
+
+    double dPowerOffVoltage = object["PowerOffVoltage"].toDouble();
+
+    if (ltc.m_filteredVolt > dPowerOffVoltage)
         return true;
 
     return false;

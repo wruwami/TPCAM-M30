2a1522a378ef33d59c35490c5fe510f7612f9d06
boseok jung <wruwami@naver.com>
2023-07-13 14:21:01 +0900

    hud 수정

--- HeadUpDisplay.cpp
+++ HeadUpDisplay.cpp
@@ -15,57 +15,92 @@ void CHeadUpDisplay::initDisplay()
     writeNumberToFile("display", 0);
     writeNumberToFile("display", 1);
     writeNumberToFile("pointer", 1);
+
+    connect(&m_DistanceTimer, SIGNAL(timeout()), this, SLOT(hideDistance()));
+    connect(&m_SpeedTimer, SIGNAL(timeout()), this, SLOT(hideSpeed()));
 }
 
-void CHeadUpDisplay::showSpeedDistance(int speed, float distance)
+void CHeadUpDisplay::showDistanceSensitivity(float fDistance, int nSensitivity)
 {
-//    writeNumberToFile("distance", 1);
-//    if(speed==0)
+    if(m_bSensitivityOn)
+    {
+        if(readNumberFromFile("distance_sensitivity") == 0 || readNumberFromFile("distance_sensitivity_unit") != m_nDistanceUnit)
+        {
+            writeNumberToFile("clear_content", 1);
+            writeNumberToFile("distance_sensitivity_unit", m_nDistanceUnit);
+            writeNumberToFile("distance", 0);
+            writeNumberToFile("distance_sensitivity", 1);
+            writeNumberToFile("pointer", 1);
+        }
+
+        writeNumberToFile("distance_sensitivity_distance_value", fDistance*10);
+        writeNumberToFile("distance_sensitivity_sensitivity_value", nSensitivity);
+    }
+    else //m_bSensitivityOn == false
+    {
+        if(readNumberFromFile("distance") == 0 || readNumberFromFile("distance_unit") != m_nDistanceUnit)
+        {
+            writeNumberToFile("clear_content", 1);
+            writeNumberToFile("distance_unit", m_nDistanceUnit);
+            writeNumberToFile("distance_sensitivity", 0);
+            writeNumberToFile("distance", 1);
+            writeNumberToFile("pointer", 1);
+        }
+
+        writeNumberToFile("distance_value", fDistance*10);
+    }
+
+    m_DistanceTimer.start(200);
+}
+
+void CHeadUpDisplay::showSpeedDistanceSensitivity(float fSpeed, float distance)
+{
+    if(m_bSensitivityOn)
+    {
+        if(readNumberFromFile("distance_sensitivity") == 0 || readNumberFromFile("distance_sensitivity_unit") != m_nDistanceUnit)
+        {
+            writeNumberToFile("clear_content", 1);
+            writeNumberToFile("distance_sensitivity_unit", m_nDistanceUnit);
+            writeNumberToFile("distance", 0);
+            writeNumberToFile("distance_sensitivity", 1);
+            writeNumberToFile("pointer", 1);
+        }
+
+//        writeNumberToFile("distance_sensitivity_distance_value", distance*10);
+    }
+    else //m_bSensitivityOn == false
+    {
+        if(readNumberFromFile("distance") == 0 || readNumberFromFile("distance_unit") != m_nDistanceUnit)
+        {
+            writeNumberToFile("clear_content", 1);
+            writeNumberToFile("distance_unit", m_nDistanceUnit);
+            writeNumberToFile("distance_sensitivity", 0);
+            writeNumberToFile("distance", 1);
+            writeNumberToFile("pointer", 1);
+        }
+
+//        writeNumberToFile("distance_value", distance*10);
+    }
+
+
+    writeNumberToFile("speed_value", fSpeed);
+
+    writeNumberToFile("speed", m_nREC);
+
+//    if((fSpeed==0) || (m_SpeedTimer.elapsed()>300))
 //    {
 //        writeNumberToFile("speed", 0);
 //    }
 //    else
 //    {
-//        writeNumberToFile("speed", 1);
+
 //    }
-
-//    writeNumberToFile("distance_value", distance);
-//    writeNumberToFile("speed_value", speed);
-
-
-    writeNumberToFile("distance", 1);
-    if((speed==0) || (m_SpeedTimer.elapsed()>500))
-    {
-        writeNumberToFile("speed", 0);
-    }
-    else
-    {
-        writeNumberToFile("speed", 2);
-    }
-
-    writeNumberToFile("distance_value", distance);
-    writeNumberToFile("speed_value", speed);
-
-    if(speed != m_fPastSpeed)
-    {
-        m_SpeedTimer.start();
-    }
-
-    m_fPastSpeed = speed;
+    m_SpeedTimer.start(200);
+    m_DistanceTimer.start(200);
 }
 
-void CHeadUpDisplay::showSpeedDistanceSensitivity(int speed, float distance, int sensitivity)
+void CHeadUpDisplay::showCaptureSpeedDistance(float fSpeed, float distance)
 {
-
-    if((speed==0) || (m_SpeedTimer.elapsed()>500))
-    {
-        writeNumberToFile("speed", 0);
-    }
-    else
-    {
-        writeNumberToFile("speed", 2);
-    }
-
     if(m_bSensitivityOn)
     {
         if(readNumberFromFile("distance_sensitivity") == 0 || readNumberFromFile("distance_sensitivity_unit") != m_nDistanceUnit)
@@ -78,8 +113,6 @@ void CHeadUpDisplay::showSpeedDistanceSensitivity(int speed, float distance, int
         }
 
         writeNumberToFile("distance_sensitivity_distance_value", distance*10);
-        writeNumberToFile("distance_sensitivity_sensitivity_value", sensitivity);
-        writeNumberToFile("speed_value", speed);
     }
     else //m_bSensitivityOn == false
     {
@@ -93,17 +126,43 @@ void CHeadUpDisplay::showSpeedDistanceSensitivity(int speed, float distance, int
         }
 
         writeNumberToFile("distance_value", distance*10);
-        writeNumberToFile("speed_value", speed);
     }
 
 
-    if(speed != m_fPastSpeed)
-    {
-        m_SpeedTimer.start();
-    }
+    writeNumberToFile("speed_value", fSpeed);
 
-    m_fPastSpeed = speed;
+    writeNumberToFile("speed", m_nREC);
+
+//    if((fSpeed==0) || (m_SpeedTimer.elapsed()>300))
+//    {
+//        writeNumberToFile("speed", 0);
+//    }
+//    else
+//    {
+
+//    }
+    m_SpeedTimer.start(600);
+    m_DistanceTimer.start(600);
 }
+void CHeadUpDisplay::hideSpeed()
+{
+    writeNumberToFile("speed", 0);
+}
+
+
+void CHeadUpDisplay::hideDistance()
+{
+    if(m_bSensitivityOn)
+    {
+        writeNumberToFile("distance_sensitivity_distance_value", 0);
+    }
+    else
+    {
+        writeNumberToFile("distance_value", 0);
+    }
+}
+
+
 
 //using QFILE
 
@@ -163,3 +222,24 @@ int CHeadUpDisplay::readNumberFromFile(QString fileName)
 //    return true;
 
 //}
+
+
+void CHeadUpDisplay::changeToSpeedCheckMode()
+{
+    m_bSensitivityOn = false;//감도를 표시 안해줍니다.
+    m_nREC = 2; //화면녹화 표시(REC)를 켜줍니다. (제한속도 이상일 경우만)
+    initDisplay();//화면을 껐다 켜고 조준선을 켜줍니다.
+}
+
+void CHeadUpDisplay::changeToAlineMode()
+{
+    m_bSensitivityOn = true;//감도를 표시 해줍니다.
+    m_nREC = 1; //화면녹화 표시(REC)를 꺼줍니다.
+    initDisplay();//화면을 껐다 켜고 조준선을 켜줍니다.
+}
+
+
+
+
+
+
--- HeadUpDisplay.h
+++ HeadUpDisplay.h
@@ -1,28 +1,40 @@
 #ifndef CHEADUPDISPLAY_H
 #define CHEADUPDISPLAY_H
 
-#include <QString>
-#include <QElapsedTimer>
+#include <Qbject>
 
-class CHeadUpDisplay
+class CHeadUpDisplay : public QObject
 {
+    Q_OBJECT
 public:
     CHeadUpDisplay();
     void initDisplay();
     bool writeNumberToFile(QString fileName, int value);
     int readNumberFromFile(QString fileName);
-    void showSpeedDistance(int speed, float distance);
-    void showSpeedDistanceSensitivity(int speed, float distance, int sensitivity);
 
+public slots:
+    void showDistanceSensitivity(float fDistance, int nSensitivity);
+    void showSpeedDistanceSensitivity(float fSpeed, float distance);
+    void showCaptureSpeedDistance(float fSpeed, float distance);
+    void hideSpeed();
+    void hideDistance();
 
-
+public:
     //Speed
-    QElapsedTimer m_SpeedTimer;
-    int m_fPastSpeed = 0;
+    QTimer m_SpeedTimer;
+    QTimer m_DistanceTimer;
+    float m_fPastSpeed = 0;
+    bool m_bIsSpeedDispOn = false;
 
     //cnrk
     bool m_bSensitivityOn = true;
+    int m_nREC = 1; //1 - REC 미표시, 2 - REC 표시
     int m_nDistanceUnit = 2;
+
+    void changeToSpeedCheckMode();
+    void changeToAlineMode();
+
+
 };
 
 #endif // CHEADUPDISPLAY_H

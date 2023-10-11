784526aaf0ddc68c1500187d2e68dfe7f429e833
Tegshbayar <tmg99@comlaser.co.kr>
2023-10-11 15:27:47 +0900

    CPU Freq 1.5로 낮춤
    CPU Freq 1.5로 낮춤

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 /home/firefly/command_change.sh
 xinput set-prop 'TSC2007 Touchscreen' 'libinput Calibration Matrix' 1.09 0 -0.05 0 1.15 -0.10 0 0 1
-
 echo "touchscreen applied"
+cpufreq-set -c 0 1 2 3 -u 1.5Ghz -g performance
+cpufreq-set -c 4 5 -u 1.5Ghz -g performance

e0b48547bf4d16b8fe8a517bf097ca5d6946efc1
Tegshbayar <tmg99@comlaser.co.kr>
2023-11-03 17:41:18 +0900

    CPU clock 1.2Ghz로 설정
    CPU clock 1.2Ghz로 설정

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ else
 fi
 xinput set-prop 'TSC2007 Touchscreen' 'libinput Calibration Matrix' 1.09 0 -0.05 0 1.15 -0.10 0 0 1
 echo "touchscreen applied"
-cpufreq-set -c 0 1 2 3 -u 1.5Ghz -g performance
-cpufreq-set -c 4 5 -u 1.5Ghz -g performance
+cpufreq-set -c 0 1 2 3 -u 1.2Ghz -g performance
+cpufreq-set -c 4 5 -u 1.2Ghz -g performance
